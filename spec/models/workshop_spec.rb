22d50302885ba1d357347365d116541b87e19b40
JTBreunissen <jantjarco@gmail.com>
2017-03-27 17:58:42 +0200

    Created Workshop model + wrote validation tests

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Workshop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a title" do
+      workshop = Workshop.new(title: "")
+      workshop.valid?
+      expect(workshop.errors).to have_key(:title)
+    end
+
+    it "is invalid with an event name of more than 75 characters" do
+      workshop = Workshop.new(title: "Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test ")
+      workshop.valid?
+      expect(workshop.errors).to have_key(:title)
+    end
+
+    it "is invalid without a description" do
+      workshop = Workshop.new(description: "")
+      workshop.valid?
+      expect(workshop.errors).to have_key(:description)
+    end
+  end
 end
