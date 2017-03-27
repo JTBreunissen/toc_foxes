87e6a4d3dcb60723e6ff07cb973a98e52dd566e0
M Sibai <sibaim@gmail.com>
2017-03-27 15:30:40 +0200

    Sponor model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327122430) do
+ActiveRecord::Schema.define(version: 20170327131202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sponsors", force: :cascade do |t|
+    t.string   "company_name"
+    t.string   "logo"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
