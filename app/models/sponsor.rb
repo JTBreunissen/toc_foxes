a0c2b470073acdb635602716b000cde9c993c264
M Sibai <sibaim@gmail.com>
2017-03-28 00:23:04 +0200

    Sponor test model added

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,2 +1,4 @@
 class Sponsor < ApplicationRecord
+
+validates :company_name, presence: true, length: {maximum: 50}
 end
--- spec/models/sponsor_spec.rb
+++ spec/models/sponsor_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Sponsor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+
+      describe "validations" do
+        it "is invalid without a company name" do
+          sponsor = Sponsor.new(company_name: "")
+          sponsor.valid?
+          expect(sponsor.errors).to have_key(:company_name)
+        end
+
+        it "is invalid with a Company name longer than 50 characters" do
+          sponsor = Sponsor.new(company_name: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. ")
+          sponsor.valid?
+          expect(sponsor.errors).to have_key(:company_name)
+        end
+      end
 end
