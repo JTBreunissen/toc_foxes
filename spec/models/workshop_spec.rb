ae086b08425688ecd5d515bbf9454b6c647e0911
JTBreunissen <jantjarco@gmail.com>
2017-03-28 15:53:27 +0200

    made changes to testfile based on feedback

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -3,26 +3,25 @@ require 'rails_helper'
 RSpec.describe Workshop, type: :model do
   describe "validations" do
     it "is invalid without a title" do
-      workshop = FactoryGirl.build(:workshop, title: "")
+      workshop = build(:workshop, title: "")
       workshop.valid?
       expect(workshop.errors).to have_key(:title)
     end
 
     it "is invalid with an event name of more than 75 characters" do
-      workshop = FactoryGirl.build(:workshop, title: "Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test ")
+      workshop = build(:workshop, title: "Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test ")
       workshop.valid?
       expect(workshop.errors).to have_key(:title)
     end
 
     it "is invalid without a description" do
-      workshop = FactoryGirl.build(:workshop, description: "")
+      workshop = build(:workshop, description: "")
       workshop.valid?
       expect(workshop.errors).to have_key(:description)
     end
 
-    it "is a valid workshop" do
-      workshop = FactoryGirl.build(:workshop)
-
+    it "has a valid factory" do
+      workshop = build(:workshop)
       expect(workshop.valid?).to be(true)
     end
   end
