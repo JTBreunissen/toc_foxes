b0d4297e1c9803ccd1b2f6c9ff8b3d66290d099d
JTBreunissen <jantjarco@gmail.com>
2017-03-28 14:21:29 +0200

    Added use of factory and included test for valid workshop

--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -3,21 +3,27 @@ require 'rails_helper'
 RSpec.describe Workshop, type: :model do
   describe "validations" do
     it "is invalid without a title" do
-      workshop = Workshop.new(title: "")
+      workshop = FactoryGirl.build(:workshop, title: "")
       workshop.valid?
       expect(workshop.errors).to have_key(:title)
     end
 
     it "is invalid with an event name of more than 75 characters" do
-      workshop = Workshop.new(title: "Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test ")
+      workshop = FactoryGirl.build(:workshop, title: "Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test ")
       workshop.valid?
       expect(workshop.errors).to have_key(:title)
     end
 
     it "is invalid without a description" do
-      workshop = Workshop.new(description: "")
+      workshop = FactoryGirl.build(:workshop, description: "")
       workshop.valid?
       expect(workshop.errors).to have_key(:description)
+    end
+
+    it "is a valid workshop" do
+      workshop = FactoryGirl.build(:workshop)
+
+      expect(workshop.valid?).to be(true)
     end
   end
 end
