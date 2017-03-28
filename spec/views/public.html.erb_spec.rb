8a265606675206e3065decb5117f87d7c370101a
FLORISSP <fjspape@gmail.com>
2017-03-28 17:24:23 +0200

    indentation

--- spec/views/public.html.erb_spec.rb
+++ spec/views/public.html.erb_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe "pages/public.html.erb" do
   it "contains \"Taste of Code\"" do
     render
-      response.should have_text("Taste of Code")
+    response.should have_text("Taste of Code")
   end
 
   it "contains \"Beginner coding workshop\"" do
     render
-      response.should have_text("Beginner coding workshop")
+    response.should have_text("Beginner coding workshop")
   end
 end
