03d936de8bd597b7d7767487115eaf51631e1fb7
ewoudtm <ewoud@ewoudtm.nl>
2017-03-28 18:07:19 +0200

    pages controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get "/pages/:page" => "pages#show"
+
   devise_for :users
+
   resources :event do
     resources :attend
   end
+
   resources :sponsor
+
   resources :workshop
+
   resources :in_company
+
 end
