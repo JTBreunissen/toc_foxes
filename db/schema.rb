3e9c0c8f0286f3a6e7730a43042ef48a4979996d
JTBreunissen <jantjarco@gmail.com>
2017-03-29 18:44:10 +0200

    created sponsor model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327140451) do
+ActiveRecord::Schema.define(version: 20170329162032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20170327140451) do
     t.boolean  "public"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "sponsors", force: :cascade do |t|
+    t.string   "company_name"
+    t.string   "logo"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/models/sponsor_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+RSpec.describe Sponsor, type: :model do
+ describe "validations" do
+   it "is invalid without a company name" do
+     sponsor = Sponsor.new(company_name: "")
+     sponsor.valid?
+     expect(sponsor.errors).to have_key(:company_name)
+   end
+   it "is valid with a company name" do
+     sponsor = Sponsor.new(company_name: "Codaisseur")
+     expect(sponsor.valid?).to eq(true)
+   end
+ end
+end
