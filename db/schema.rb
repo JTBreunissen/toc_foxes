c4d8cdede9d80f20240d21859917e03b5165352d
ewoudtm <ewoud@ewoudtm.nl>
2017-03-29 18:13:58 +0200

    further updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328120749) do
+ActiveRecord::Schema.define(version: 20170327140451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.string   "image"
+    t.string   "location"
+    t.integer  "spaces_available"
+    t.boolean  "public"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
