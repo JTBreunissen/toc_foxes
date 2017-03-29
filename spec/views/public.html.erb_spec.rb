14d66c624c50413b22acafaa69b6308cacef66d8
FLORISSP <fjspape@gmail.com>
2017-03-29 10:43:19 +0200

    Added What Will You Learn section + tests

--- spec/views/public.html.erb_spec.rb
+++ spec/views/public.html.erb_spec.rb
@@ -10,4 +10,14 @@ describe "pages/public.html.erb" do
     render
     response.should have_text("Beginner coding workshop")
   end
+
+  it "contains \"Professional developers technologies\"" do
+    render
+    response.should have_text("Professional developers technologies")
+  end
+
+  it "contains \"JavaScript is a programming language\"" do
+    render
+    response.should have_text("JavaScript is a programming language")
+  end
 end
