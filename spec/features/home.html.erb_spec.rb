86f75b2d57359f23a644bedc4eca96952f820e2c
lars3016 <lars.v.ommeren@gmail.com>
2017-03-29 17:39:24 +0200

    view homepage

---
+++ spec/features/home.html.erb_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature "links homepage", type: :feature do
+
+    describe "link Join toc" do
+      it "links to /attend" do
+
+        visit root_url
+        click_on "JOIN TOC"
+
+        expect visit "/attend"
+
+      end
+    end
+
+    describe "link INCOMPANY" do
+      it "links to /in_company" do
+
+        visit root_url
+        click_on "INCOMPANY"
+
+        expect visit "/in_company"
+
+      end
+    end
+
+    describe "link HOST TOC" do
+      it "links to /sponsor" do
+
+        visit root_url
+        click_on "HOST TOC"
+
+        expect visit "/sponsor"
+
+      end
+    end
+end
