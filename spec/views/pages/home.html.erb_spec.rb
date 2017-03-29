e4d6962bdb401423f646df3226e177f0ad491d90
lars3016 <lars.v.ommeren@gmail.com>
2017-03-29 18:44:39 +0200

    did the adjustments for the home page

--- spec/views/pages/home.html.erb_spec.rb
+++ spec/views/pages/home.html.erb_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe "pages/home.html.erb", type: :view do
   describe "rendering home page" do
     it "contains \"Pages#home\"" do
       render
-      response.should have_text("Taste of code")
-      response.should have_text("Beginner coding workshop")
+        response.should have_text("Taste of code")
+        response.should have_text("Beginner coding workshop")
     end
   end
 end
