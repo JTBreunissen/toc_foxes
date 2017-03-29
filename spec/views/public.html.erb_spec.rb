e72cdb5da758a2eee6e31b51799747cc7265f7ff
M Sibai <sibaim@gmail.com>
2017-03-29 11:52:04 +0200

    ready for the day section added

--- spec/views/public.html.erb_spec.rb
+++ spec/views/public.html.erb_spec.rb
@@ -10,4 +10,14 @@ describe "pages/public.html.erb" do
     render
     response.should have_text("Beginner coding workshop")
   end
+
+  it "contains Get Ready for the Day" do
+    render
+    response.should have_text("Get Ready for the Day")
+  end
+
+  it "Know your Tools" do
+    render
+    response.should have_text("Know your Tools")
+  end
 end
