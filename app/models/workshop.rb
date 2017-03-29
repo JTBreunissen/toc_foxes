20d701b23fccdd217bef7d6f3ef7fa0f67a1fbc6
JTBreunissen <jantjarco@gmail.com>
2017-03-29 10:29:22 +0200

    added model tests for review

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,5 +1,4 @@
 class Workshop < ApplicationRecord
-
   validates :title, presence: true, length: { maximum: 75 }
   validates :description, presence: true
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a reviewer" do
+      review = build(:review, reviewer: "")
+      review.valid?
+      expect(review.errors).to have_key(:reviewer)
+    end
+
+    it "is invalid without a review/ description" do
+      review = build(:review, review: "")
+      review.valid?
+      expect(review.errors).to have_key(:review)
+    end
+
+    it "has a valid factory" do
+      review = build(:review)
+      expect(review.valid?).to be(true)
+    end
+  end
 end
