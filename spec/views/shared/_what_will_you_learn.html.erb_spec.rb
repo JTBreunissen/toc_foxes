fdc1a3fe0afe9e9c33b86fb5f999c59b422a6398
FLORISSP <fjspape@gmail.com>
2017-03-29 16:47:35 +0200

    Restructured the spec

--- spec/views/shared/_what_will_you_learn.html.erb_spec.rb
+++ spec/views/shared/_what_will_you_learn.html.erb_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
 
-describe "pages/shared/_what_will_you_learn.html.erb" do
-  it "contains \"Professional developers technologies\"" do
+describe "shared/_what_will_you_learn.html.erb" do
+  it "contains content" do
     render
     rendered.should have_text("Professional developers technologies")
-  end
-
-  it "contains \"JavaScript is a programming language\"" do
-    render
     rendered.should have_text("JavaScript is a programming language")
   end
 end
