b9b78572c71aec014ecd97b801ae38a50acf8c98
M Sibai <sibaim@gmail.com>
2017-03-30 17:08:10 +0200

    merge conflict

--- spec/views/shared/_ready_for_the_day.html.erb_spec.rb
+++ spec/views/shared/_ready_for_the_day.html.erb_spec.rb
@@ -4,19 +4,27 @@ describe "shared/_ready_for_the_day.html.erb" do
 
   it "contains Get Ready for the Day" do
     render
+<<<<<<< 7fad34b7e9a52e1c1cb820aa6f56d9f11477ec42
 <<<<<<< 20329f9010030550283b7cd1db861881be317166
     expect(response).to have_text("Get Ready for the Day")
 =======
     response.should have_text("Get Ready for the Day")
 >>>>>>> merge conflict solved
+=======
+    expect(response).to have_text("Get Ready for the Day")
+>>>>>>>  changed based on review
   end
 
   it "Know your Tools" do
     render
+<<<<<<< 7fad34b7e9a52e1c1cb820aa6f56d9f11477ec42
 <<<<<<< 20329f9010030550283b7cd1db861881be317166
     expect(response).to have_text("Know your Tools")
 =======
     response.should have_text("Know your Tools")
 >>>>>>> merge conflict solved
+=======
+    expect(response).to have_text("Know your Tools")
+>>>>>>>  changed based on review
   end
 end
