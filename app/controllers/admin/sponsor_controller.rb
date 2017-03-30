e001b5f575241e887299b85dc8e35eaf2c6e654b
JTBreunissen <jantjarco@gmail.com>
2017-03-30 21:09:55 +0200

    made index view sponsors

--- app/controllers/admin/sponsor_controller.rb
+++ app/controllers/admin/sponsor_controller.rb
@@ -1,5 +1,12 @@
 class Admin::SponsorController < ApplicationController
+before_action :set_sponsors
+
   def index
+  end
+
+  private
+
+  def set_sponsors
     @sponsors = Sponsor.all
   end
 end
