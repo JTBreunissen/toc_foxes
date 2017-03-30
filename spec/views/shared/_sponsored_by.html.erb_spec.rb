34f93bd464747b4b7aa51b9bd1ed70e0b62fee07
JTBreunissen <jantjarco@gmail.com>
2017-03-30 20:05:23 +0200

    adjusted testfile

--- spec/views/shared/_sponsored_by.html.erb_spec.rb
+++ spec/views/shared/_sponsored_by.html.erb_spec.rb
@@ -4,6 +4,5 @@ describe "shared/_sponsored_by.html.erb" do
   it "contains content" do
     render
     expect(rendered).to have_text("Proudly supported by")
-    expect(rendered).to have_text("J.P. Morgan")
   end
 end
