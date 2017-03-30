9e51baa8232c758c3bc3b4139d03bd9db5778ff3
JTBreunissen <jantjarco@gmail.com>
2017-03-30 23:08:04 +0200

    setting up views for admin/sponsor

--- app/controllers/admin/sponsor_controller.rb
+++ app/controllers/admin/sponsor_controller.rb
@@ -1,12 +1,34 @@
 class Admin::SponsorController < ApplicationController
 before_action :set_sponsors
+before_action :set_sponsor, only: [:show, :edit, :update]
 
   def index
+  end
+
+  def create
+    @sponsor = Sponsor.new(sponsor_params)
+
+    if @sponsor.save
+      redirect_to admin_sponsor_index, :notice => "Sponsor successfully created"
+    else
+      redirect_to admin_sponsor_index, :alert => "Please fill in all required fields"
+    end
+  end
+
+  def show
   end
 
   private
 
   def set_sponsors
     @sponsors = Sponsor.all
+  end
+
+  def set_sponsor
+    @sponsor = Sponsor.find(params[:id])
+  end
+
+  def sponsor_params
+    params.require(:sponsor).permit(:company_name, :logo)
   end
 end
