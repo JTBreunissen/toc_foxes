70c95e310f861c37c0e4b6a5243b128ffc61ae7a
M Sibai <sibaim@gmail.com>
2017-03-30 17:23:56 +0200

    change indentation and test

--- spec/views/shared/company_banner.html.erb_spec.rb
+++ spec/views/shared/company_banner.html.erb_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe "shared/_company_banner.html.erb" do
   it "Taste of Code" do
     render
-    response.should have_text("Taste of Code")
+    expect(response).to have_text("Taste of Code")
   end
 
   it "Begginer Coding workshop" do
     render
-    response.should have_text("Beginner Coding Workshop")
+    expect(response).to have_text("Beginner Coding Workshop")
   end
 end
