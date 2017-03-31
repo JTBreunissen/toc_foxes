a04fe8405a49291f2c196e85dddcf9e9a3090f5d
lars3016 <lars.v.ommeren@gmail.com>
2017-03-31 14:33:29 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-
   get '/', to: 'pages#home'
   get '/attend', to: 'pages#attend'
   get '/in_company', to: 'pages#in_company'
@@ -9,9 +8,8 @@ Rails.application.routes.draw do
   get '/admin/event/new', to: "event#new", as: :new_event
   get '/admin/event/:id', to: 'event#show', as: :event
   get '/admin/event/:id/edit', to: 'event#edit', as: :edit_event
-
  patch "admin/events" => "event#update"
-
+ devise_for :users
 namespace :admin do
   resources :attend, :in_company, :sponsors, :events
 end
