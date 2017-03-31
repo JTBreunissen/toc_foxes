9c757754ac8b0ff5b49cc78bdaec2e5d264383a4
lars3016 <lars.v.ommeren@gmail.com>
2017-03-31 14:35:29 +0200

    Update _ready_for_the_day.html.erb_spec.rb

--- spec/views/shared/_ready_for_the_day.html.erb_spec.rb
+++ spec/views/shared/_ready_for_the_day.html.erb_spec.rb
@@ -1,30 +1,32 @@
-require 'rails_helper'
-
-describe "shared/_ready_for_the_day.html.erb" do
-
-  it "contains Get Ready for the Day" do
-    render
-<<<<<<< 7fad34b7e9a52e1c1cb820aa6f56d9f11477ec42
-<<<<<<< 20329f9010030550283b7cd1db861881be317166
-    expect(response).to have_text("Get Ready for the Day")
-=======
-    response.should have_text("Get Ready for the Day")
->>>>>>> merge conflict solved
-=======
-    expect(response).to have_text("Get Ready for the Day")
->>>>>>>  changed based on review
-  end
-
-  it "Know your Tools" do
-    render
-<<<<<<< 7fad34b7e9a52e1c1cb820aa6f56d9f11477ec42
-<<<<<<< 20329f9010030550283b7cd1db861881be317166
-    expect(response).to have_text("Know your Tools")
-=======
-    response.should have_text("Know your Tools")
->>>>>>> merge conflict solved
-=======
-    expect(response).to have_text("Know your Tools")
->>>>>>>  changed based on review
-  end
-end
+<section style="background-color:#e6e6ff;">
+  <div id="apps_section" class="container">
+    <div class="jumbotron">
+      <div class="row">
+      <h1 style="text-align:center;">Get Ready for the Day</h1>
+      </div>
+      <div id="tools" class="row">
+      <h2 style="text-align:center;">Know your Tools</h2>
+      </div>
+      <div id="apps" class="row">
+        <div id="atom" class="col-md-4">
+        <span>Atom</span>
+        <a title="Atom" href="https://atom.io" target="_blank">
+        <img src="https://cdn-business.discourse.org/uploads/github_atom/490/d8548f4ce56f1599.png" width="50" height="50"/>
+        </a>
+        </div>
+        <div id="chrome" class="col-md-4">
+        <span>Chrome</span>
+        <a title="Chrome" href="https://www.google.com/chrome/" target="_blank">
+        <img src="https://www.google.com/images/icons/product/chrome-32.png" width="50" height="50"/>
+        </a>
+        </div>
+        <div id="slack" class="col-md-4">
+        <span>Slack</span>
+        <a title="Slack" href="https://slack.com/" target="_blank">
+        <img src="http://iosicongallery.com/img/512/slack-2014.png" width="50" height="50"/>
+        </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section
